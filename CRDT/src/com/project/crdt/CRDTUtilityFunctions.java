1784066e4a9dca840d62f19ba070d953813c039d
matthew-f-morris <36605894+matthew-f-morris@users.noreply.github.com>
2019-03-21 15:21:27 +0000

    Finished algo implementation
    is it right? well were gunna find out on the next episode of Git Commit!!

--- CRDT/src/com/project/crdt/CRDTUtilityFunctions.java
+++ CRDT/src/com/project/crdt/CRDTUtilityFunctions.java
@@ -1,33 +1,22 @@
 package com.project.crdt;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public final class CRDTUtilityFunctions {
 	
+	static Random rand = new Random();
+	
 	private CRDTUtilityFunctions() {}
-	
-	/*
-	 *	Compare atom identifiers, returns 1 if x > y, returns -1 if y > x,
-	 * 	returns 0 if x == y 
-	 */
-	
-	public static int compareAtomIdentifiers(AtomIdentifier x, AtomIdentifier y) {
-		System.out.println("ur mum");
-		return comparePos(x.position, y.position);
-	}
-	
-	public static int comparePosition(Position posx, Position posy) {
-		// implement 
-	}
-	
-	public static void generateLinePositions(Position posP, Position posQ, int numberLines, String siteId) {
+		
+	public static ArrayList<Position> generateLinePositions(Position posP, Position posQ, int numberLines, String siteId) {
 		
 		ArrayList<Position> positions = new ArrayList<Position>();
+		
 		int index = 0;
 		int interval = 0;
 		
-		while(interval < numberLines) {
-			
+		while(interval < numberLines) {			
 			index++;
 			interval = prefix(posQ, index) - prefix(posP, index);
 		}
@@ -35,9 +24,32 @@ public final class CRDTUtilityFunctions {
 		int step = interval / numberLines;
 		int r = prefix(posP, index);
 		
-		for(int i = 0; i < numberLines; i++) {
-			positions.add(constructPosition(r, posP, posQ, siteId));
+		for(int i = 0; i < numberLines; i++) {			
+			
+			int low = 1;
+			int high = step;
+			int random = rand.nextInt(high - low) + low;
+			
+			positions.add(constructPosition(r + random, posP, posQ, siteId));
+			r = r + step;
 		}
+		
+		return positions;
+	}
+	
+	public static Position generateLinePosition(Position posP, Position posQ, String siteId) {
+		
+		int interval = 0;
+		int index = 0;
+		
+		while(interval < 1) {
+			index++;
+			interval = prefix(posQ, index) - prefix(posP, index);
+		}
+		
+		int r = prefix(posP, index);
+		
+		return constructPosition(r, posP, posQ, siteId);
 	}
 	
 	public static Position constructPosition(int r, Position posP, Position posQ, String oldId) {
@@ -68,12 +80,36 @@ public final class CRDTUtilityFunctions {
 
 	public static int prefix(Position pos, int index) {
 		
-		if(pos.ids.size() < i)
+		if(pos.ids.size() < index)
 			return 0;
 		else {
-			pos.ids.get(index)
+			return pos.ids.get(index).position;
 		}
 	}
 }
 
-//The function prefix(p,i) returns a number in base MAXINT which each digits is pi.pos the integers of the first ith identifiers of p (filled with 0 if |p|<  i).
+
+
+
+
+
+
+/*
+// *	Compare atom identifiers, returns 1 if x > y, returns -1 if y > x,
+// * 	returns 0 if x == y 
+// */
+//
+//public static int compareAtomIdentifiers(AtomIdentifier x, AtomIdentifier y) {
+//	System.out.println("ur mum");
+//	return comparePos(x.position, y.position);
+//}
+//
+//public static int comparePosition(Position posx, Position posy) {
+//	// implement 
+//}
+//
+///*
+// *	Implementation of generateLinePositions(p, q, N, s) from paper
+// */
+
+
